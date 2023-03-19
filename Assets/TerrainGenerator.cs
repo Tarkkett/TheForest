a570ce668c906fd131678d0b7905bc1bd41dd911
Tarkkett <88042679+Tarkkett@users.noreply.github.com>
2023-03-19 14:09:09 +0200

    Version1

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -8,10 +8,13 @@ using UnityEngine.SceneManagement;
 public class TerrainGenerator : MonoBehaviour
 {
     public int size = 256; //map size
+    public float islandSize = 90;
     public int maxHeight = 64;
     public bool autoGenerate = true;
     private Terrain terrain;
     public float mountainSize = 50;
+    public float hillSize = 50;
+    public float bumpSize = 50;
 
     private void Start()
     {
@@ -34,8 +37,20 @@ public class TerrainGenerator : MonoBehaviour
         {
             for (int x = 0; x < size; x++)
             {
+                //distance from center
+                var distance = Vector2.Distance(new Vector2(size / 2, size / 2), new Vector2(x, y));
+                distance /= islandSize;
+                distance = math.remap(0, size, 1, 0, distance);
+
+
+                //generate height
                 var pos = new float2(x, y) / mountainSize;
-                var height = noise.pnoise(pos, size);
+                var height =  math.remap(-1, 1, 0, 1, noise.pnoise(pos, size));
+                height += math.remap(-1, 1, 0, 1, noise.pnoise(pos*2, size));
+                height += math.remap(-1, 1, 0, 1, noise.pnoise(pos*2, size));
+                height *= distance;
+
+                //set height
                 heightMap[x, y] = height;
             }
         }
